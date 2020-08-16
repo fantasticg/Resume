e21562f71ec0d49713b5560d86e008d72bc01cd9
FANTASTICRCT <fantasticg@naver.com>
2020-08-16 12:25:42 +0900

    #학력/경력 액티비티

--- app/src/main/java/com/example/heekyungyeom_resume/EduExperienceActivity.java
+++ app/src/main/java/com/example/heekyungyeom_resume/EduExperienceActivity.java
@@ -4,11 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.heekyungyeom_resume.ui.main.MainFragment;
 import com.example.heekyungyeom_resume.ui.main.SkillsFragment;
 import com.example.heekyungyeom_resume.ui.main.dummy.EducationContent;
 
-public class MainActivity extends AppCompatActivity implements SkillsFragment.OnListFragmentInteractionListener {
+public class EduExperienceActivity extends AppCompatActivity implements SkillsFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +16,6 @@ public class MainActivity extends AppCompatActivity implements SkillsFragment.On
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.main_fragment, SkillsFragment.newInstance(1))
-                    .commitNow();
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.description_fragment, MainFragment.newInstance())
                     .commitNow();
         }
     }
